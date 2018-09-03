516d79b28fddfeb784b47118d047108319537138
ashokbhayal <36731969+ashokbhayal@users.noreply.github.com>
2018-09-03 23:13:49 +0530

    Change in file name

--- pthread.c
+++ pthread.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 
+long long sum = 0;
+
+void* sum_runner(void* arg)
+{
+	long long *limit_ptr = (long long*)arg;
+}
 int main()
 {
   printf("This is the test code\n");
