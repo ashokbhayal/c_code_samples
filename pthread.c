6bdbc677397126e8558cc3eb96e4c3c10dced09c
ashokbhayal <36731969+ashokbhayal@users.noreply.github.com>
2018-09-04 00:16:13 +0530

    first working sample code

--- pthread.c
+++ pthread.c
@@ -7,10 +7,39 @@ long long sum = 0;
 void* sum_runner(void* arg)
 {
 	long long *limit_ptr = (long long*)arg;
+	long long limit = *limit_ptr;
+
+	for (int i = 0; i < limit; i++)
+	{
+		sum += i;
+	}
+
+	pthread_exit(0);
 }
-int main()
+int main(int argc, char ** argv)
 {
-  printf("This is the test code\n");
+	if(argc <2)
+	{
+		printf("Need a numner\n");
+		exit(-1);
+	}
+
+	long long limit = atoll(argv[1]);
+
+	pthread_t tid;
+
+	pthread_attr_t attr;
+	pthread_attr_init(&attr);
+
+	pthread_create(&tid, &attr, sum_runner, &limit);
+
+	printf("Waiting for the calculation to get over\n");
+
+	pthread_join(tid, NULL);
+
+	printf("Calculation done\n");
+	
+	printf("Sum is %lld\n", sum);
 
   return 0;
 }
